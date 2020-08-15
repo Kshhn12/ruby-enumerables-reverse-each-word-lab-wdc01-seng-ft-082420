3f59b6743d25c9ea1662bd72f592a73b0f3e7030
Kevin Sheehan <kshhn12@gmail.com>
2020-08-15 14:03:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string_array = string.split(' ')
   new_string = ""
-  string_array.each do |word| 
+  string_array.collect do |word| 
     if(word == string_array[0])
       new_string = "#{new_string}#{word.reverse}"
     else
