f586940232f38199eb3bebf3ce49020e3c41416e
Kevin Sheehan <kshhn12@gmail.com>
2020-08-15 13:54:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   string_array = string.split(' ')
   new_string = " "
   string_array.each do |word| 
-    new_string + word.reverse
+    new_string = new_string + word.reverse
   end
   new_string
 end
