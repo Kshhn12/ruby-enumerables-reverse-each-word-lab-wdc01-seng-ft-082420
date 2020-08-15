cb77870c26582d1b42b7821a0708c342f84afb2d
Kevin Sheehan <kshhn12@gmail.com>
2020-08-15 13:50:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,7 @@
 def reverse_each_word(string)
-  string.split(' ')
-  
+  string_array = string.split(' ')
+  string_array.each do |word|
+    word_array = word.split('')
+    num_of_chars = word_array.length
+    word_array.each do |char| 
 end
